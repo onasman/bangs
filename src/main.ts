c3e9bc4dd65d55d1a7ee2c9528d214e5278a67ef
onasman <oskar.nasman@eqtpartners.com>
2025-02-27 22:51:15 +0100

    update url

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,17 @@ import "./global.css";
 
 function noSearchDefaultPageRender() {
   const app = document.querySelector<HTMLDivElement>("#app")!;
+  const url = new URL(window.location.href);
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
+        <h1>Bangs</h1>
         <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
         <div class="url-container"> 
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="${url.origin}?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -20,13 +21,6 @@ function noSearchDefaultPageRender() {
           </button>
         </div>
       </div>
-      <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
-      </footer>
     </div>
   `;
 
